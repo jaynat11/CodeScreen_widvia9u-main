ea9851306328649e124bd27e81ab0c4c4d43de09
jaynat11 <jayadevinatarajan11@gmail.com>
2023-07-18 23:06:13 +0200

    Implemented final commit unit tests for bestselling books statistics

--- test_bestselling_books_stats_generator.py
+++ test_bestselling_books_stats_generator.py
@@ -1,18 +1,15 @@
 import sys
 import os
 import pytest
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "books")))
+
 from bestselling_books_stats_generator import BestsellingBooksStatsGenerator
 from books_csv_parser import BooksCSVParser
-<<<<<<< HEAD
 
-
-=======
->>>>>>> unittest
 fiction_genre = "Fiction"
 non_fiction_genre = "Non Fiction"
 ranked_year = 2018
-
 
 @pytest.fixture
 def bestselling_books_stats_generator_setup():
